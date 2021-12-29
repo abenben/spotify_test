324992abdffa24e9c865e2c58f512a6462ead65b
abenben <abenbenben@gmail.com>
2021-12-29 20:16:12 +0900

    修正

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ redirect_uri = os.environ.get("REDIRECT_URI")
 def show_tracks(results):
     for i, item in enumerate(results["items"]):
         track = item["track"]
-        print("   %d %32.32s %s" % (i, track["artists"][0]["name"], track["name"]))
+        print("   %d %32.32s %s¥n" % (i, track["artists"][0]["name"], track["name"]))
 
 
 if __name__ == "__main__":
@@ -32,9 +32,8 @@ if __name__ == "__main__":
 
     for playlist in playlists["items"]:
         if playlist["owner"]["id"] == user_id:
-            print()
-            print(playlist["name"])
-            print("  total tracks", playlist["tracks"]["total"])
+            print("%s¥n" % playlist["name"])
+            print("  total tracks %s¥n" % playlist["tracks"]["total"])
 
             results = sp.playlist(playlist["id"], fields="tracks,next")
             tracks = results["tracks"]
