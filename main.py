6f7f0c3a65cc3eb55ad221155f8a9fe050bb04bd
abenben <abenbenben@gmail.com>
2023-03-18 11:06:00 +0900

    修正

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+# osモジュールをインポート
 import os
+# spotipyパッケージをインポート
 import spotipy
-import json
 from spotipy.oauth2 import SpotifyOAuth
 
+# 環境変数からクライアントID、クライアントシークレット、リダイレクトURIを取得
 client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
 redirect_uri = os.environ.get("REDIRECT_URI")
 
-
+# 曲を表示するための関数を定義
 def show_tracks(results):
     for i, item in enumerate(results["items"]):
         track = item["track"]
@@ -16,6 +18,7 @@ def show_tracks(results):
 
 if __name__ == "__main__":
 
+    # SpotifyOAuthオブジェクトを使用して、SpotifyAPIに認証されたSpotipyオブジェクトを作成
     sp = spotipy.Spotify(
         auth_manager=SpotifyOAuth(
             client_id=client_id,
@@ -25,23 +28,38 @@ if __name__ == "__main__":
         )
     )
 
-    res = sp.devices()
-    print(json.dumps(res, indent=2))
+    # ユーザーのSpotifyデバイスのリストを取得し、アクティブなデバイスを見つける
+    devices = sp.devices()
+    active_device: None = next(
+        (device for device in devices["devices"] if device["is_active"]), None
+    )
+    # アクティブなデバイスが見つかった場合は、そのデバイスで曲を再生する
+    if active_device:
+        print("Active device found: {}".format(active_device["name"]))
+        sp.start_playback(device_id=active_device["id"], uris=["spotify:track:2AVbKUuvvzbPK6NIZZZvcY"])
+    else:
+        print("No active device found.")
 
-    # playlists = sp.current_user_playlists()
-    # user_id = sp.me()["id"]
-    #
-    # for playlist in playlists["items"]:
-    #     if playlist["owner"]["id"] == user_id:
-    #         print("%s¥n" % playlist["name"])
-    #         print("  total tracks %s¥n" % playlist["tracks"]["total"])
-    #
-    #         results = sp.playlist(playlist["id"], fields="tracks,next")
-    #         tracks = results["tracks"]
-    #         show_tracks(tracks)
-    #
-    #         while tracks["next"]:
-    #             tracks = sp.next(tracks)
-    #             show_tracks(tracks)
+    # ユーザーのプレイリストのリストを取得し、プレイリストごとに曲を表示する
+    playlists = sp.current_user_playlists()
+    user_id = sp.me()["id"]
 
+    for playlist in playlists["items"]:
+        if playlist["owner"]["id"] == user_id:
+            print("%s¥n" % playlist["name"])
+            print(f"  total tracks {playlist['tracks']['total']}¥n")
+
+            results = sp.playlist(playlist["id"], fields="tracks,next")
+            tracks = results["tracks"]
+            show_tracks(tracks)
+
+            while tracks["next"]:
+                tracks = sp.next(tracks)
+                show_tracks(tracks)
+
+            # 指定されたトラックを再生する
+            sp.start_playback(uris=["spotify:track:2AVbKUuvvzbPK6NIZZZvcY"])
+            break
+
+    # 最後に指定されたトラックを再生する
     sp.start_playback(uris=["spotify:track:2AVbKUuvvzbPK6NIZZZvcY"])
